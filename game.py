bb4cf00e8bbb0b8343fa9fb6ca6650f4e3b5487d
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-08 00:43:47 +0000

    Display player greeting

--- game.py
+++ game.py
@@ -1,3 +1,20 @@
 """A number-guessing game."""
+import random
 
-print("hi")
+random_number = random.randint(1,100)
+
+print ("Hello!")
+name = input("What is your name?")
+guess = 0
+
+while guess != random_number:
+    guess = int(input("What is your guess between 1 to 100?"))
+    if guess > random_number:
+        print("your guess is too high")
+        continue
+    elif guess < random_number:
+        print("your guess is too low")
+        continue
+    else:
+        print("You guessed correctly!")
+
