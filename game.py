3425cf09cd0f1e0a5f9cdf968282b89d6f3ae084
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-08 01:13:56 +0000

    Guess is not an integer

--- game.py
+++ game.py
@@ -9,10 +9,15 @@ guess = 0
 number_of_gusses = 0
 
 while guess != random_number:
-    guess = int(input("Hi What is your guess between 1 to 100?"))
+    guess = input("Hi What is your guess between 1 to 100?")
+    try:
+        guess = int(guess)
+    except:
+        print("This is not a number, try again.")
+        continue
     if guess > 100:
         print("your guess is out of range")
-        continue
+        continue 
     elif guess > random_number:
         print("your guess is too high")
         number_of_gusses += 1
