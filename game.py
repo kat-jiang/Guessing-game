616770d0862987005dcc1516f8f736b5e1594ca5
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-08 01:01:54 +0000

    Out of Range

--- game.py
+++ game.py
@@ -6,15 +6,20 @@ random_number = random.randint(1,100)
 print ("Hello!")
 name = input("What is your name?")
 guess = 0
+number_of_gusses = 0
 
 while guess != random_number:
-    guess = int(input("What is your guess between 1 to 100?"))
-    if guess > random_number:
+    guess = int(input("Hi What is your guess between 1 to 100?"))
+    if guess > 100:
+        print("your guess is out of range")
+        continue
+    elif guess > random_number:
         print("your guess is too high")
+        number_of_gusses += 1
         continue
     elif guess < random_number:
         print("your guess is too low")
+        number_of_gusses += 1
         continue
     else:
-        print("You guessed correctly!")
-
+        print(f"You guessed correctly! It took you {number_of_gusses} guesses.")
